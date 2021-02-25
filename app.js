44f5e3082707919ea4fb133f18d329e400e60ff8
Jon Smith <aardvarkjon94@yahoo.com>
2021-02-24 18:02:32 -0600

    Refactored JS code and added comments

--- app.js
+++ app.js
@@ -1,48 +1,40 @@
-function Book(title, author, pages, has_read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.has_read = has_read;
+//                Book Object (Will want to use later) -- Leave Commented out for now
 
-    
-}
+// function Book(title, author, pages, has_read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.has_read = has_read;   
+// }
 
-
-Book.prototype.info = function () {
-    return this.title + " by " + this.author + "-" + this.pages + " " + this.has_read ;
- }
- const super_powereds =  new Book("SuperPowereds","Drew Hayes", "lots", "False")
-
- console.log(super_powereds.info())
-
-//Appending to the booklist
-
-const bookList = document.querySelector('#book-list');
+// Book.prototype.info = function () {
+//     return this.title + " by " + this.author + "-" + this.pages + " " + this.has_read ;
+//  }
+//  const super_powereds =  new Book("SuperPowereds","Drew Hayes", "lots", "False")
+//---------------------------------------------------------------------------------------------------//
 
 
-bookList.innerHTML += super_powereds.title + " by " + super_powereds.author + "-" + super_powereds.pages + " " + super_powereds.has_read;
-
-
-const addBtn = document.querySelector("#add-book button");
-
+//DOM Elemet Variables for adding books
+const addBtn = document.forms['add-book'];
 const title = document.getElementById("book-title");
-
 const bookListItem = document.querySelector('#book-list ul')
+//DOM Element Variables for deleting books
+const list = document.querySelector("#book-list ul");
 
-addBtn.addEventListener('click', function (e){
+//Event Listener for adding a new book
+addBtn.addEventListener('submit', function (e){
+    //Preventing the page from reloading on submit
     e.preventDefault();
     if(title.value !== "") {
         bookListItem.innerHTML += ("<li><span class='name'> " + title.value + "</span> <span class='delete'>delete</span></li>");
     } else {
         return alert("Please enter a title");
     }
-    
+    //Once I hit submit, clear the text
     title.value="";
 })
 
-
-const list = document.querySelector("#book-list ul");
-
+//Event Listener for the delet buttons
 list.addEventListener("click" , function(e) {
     if(e.target.className == 'delete'){
         const li = e.target.parentElement;

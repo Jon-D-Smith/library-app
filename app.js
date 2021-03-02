ad7c6237a4a239742eada669bc865f990d4546e5
Jon Smith <aardvarkjon94@yahoo.com>
2021-03-01 19:32:07 -0600

    Added search filter for book titles

--- app.js
+++ app.js
@@ -65,6 +65,27 @@ list.addEventListener("click" , deleteFun)
 
 
 
+//filtering through the list
+
+const searchBar = document.forms['search-books'].querySelector('input');
+
+searchBar.addEventListener('keyup', function(e){
+    const searchValue = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book) {
+        const title = book.firstElementChild.textContent
+        // const author = book.lastElementChild.textContent
+        if(title.toLowerCase().indexOf(searchValue) != -1){
+            book.style.display = "block";
+            
+        } else {
+            book.style.display = "none";
+        }
+    })
+})
+
+
+
 
 
 //                Book Object (Will want to use later) -- Leave Commented out for now

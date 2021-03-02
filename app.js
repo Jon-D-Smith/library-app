6b060c750485f680a0654667d9b83ba6026312e9
Jon Smith <aardvarkjon94@yahoo.com>
2021-03-01 22:08:18 -0600

    Added search for author name

--- app.js
+++ app.js
@@ -74,9 +74,11 @@ searchBar.addEventListener('keyup', function(e){
     const books = list.getElementsByTagName('li');
     Array.from(books).forEach(function(book) {
         const title = book.firstElementChild.textContent
-        // const author = book.lastElementChild.textContent
-        if(title.toLowerCase().indexOf(searchValue) != -1){
+        const author = book.lastElementChild.textContent
+        
+        if(title.toLowerCase().indexOf(searchValue) != -1 || author.toLowerCase().indexOf(searchValue) != -1 ){
             book.style.display = "block";
+            
             
         } else {
             book.style.display = "none";
